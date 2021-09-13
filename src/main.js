5865f664baf23d4a6f17d03324f926ccf4fb70f9
David Hecker <HECKERDW@GMAIL.COM>
2021-09-13 13:39:41 -0700

    Added hornedbeasts and props.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,38 @@ import { Component } from 'react';
 class Main extends Component {
   
   render() {
-    return <h1>Main is maining</h1>
+    return (
+      <>
+        <h1>Main is maining</h1>
+        <HornedBeasts
+          imgSrc="https://place-hold.it/300x500"
+          alt="Horned Image"
+          title="idk horned title"
+        />
+        <HornedBeasts
+          imgSrc="https://place-hold.it/300x500"
+          alt="Horned Image"
+          title="idk horned title"
+        />
+      </>
+    );
+  }
+}
+
+class HornedBeasts extends Component {
+
+  render() {
+    return (
+      <>
+        <h2>Horned title</h2>
+        <img
+          src={this.props.imgSrc}
+          alt={this.props.alt}
+          title={this.props.title}
+        />
+        <p>p description of the beast</p>
+      </>
+    );
   }
 }
 
