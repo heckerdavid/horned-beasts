eaad4391994b871b61ba68abed078b1b979e8e42
David Hecker <HECKERDW@GMAIL.COM>
2021-09-14 15:12:25 -0700

    Completed dynamic img rendering from json. Added click tracking.

--- README.md
+++ README.md
@@ -68,3 +68,37 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+#### Feature tracking
+
+Number and name of feature: Feature #1: Display images
+
+Estimate of time needed to complete: 40 min
+
+Start time: 1425
+
+Finish time: 1435
+
+Actual time needed to complete: 10 min
+
+<!--  -->
+
+Number and name of feature: Feature #2: Allow users to vote for their favorite beast
+
+Estimate of time needed to complete: 40 min
+
+Start time: 1440
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- src/main.js
+++ src/main.js
@@ -1,49 +1,5 @@
 import { Component } from 'react';
-
-
-// let beastsArray = [
-//   {
-//     image_url:
-//       "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-//     title: "UniWhal",
-//     description: "A unicorn and a narwhal nuzzling their horns",
-//     keyword: "narwhal",
-//     horns: 1,
-//   },
-
-//   {
-//     image_url:
-//       "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-//     title: "Rhino Family",
-//     description: "Mother (or father) rhino with two babies",
-//     keyword: "rhino",
-//     horns: 2,
-//   },
-
-//   {
-//     image_url:
-//       "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-//     title: "Unicorn Head",
-//     description: "Someone wearing a creepy unicorn head mask",
-//     keyword: "unicorn",
-//     horns: 1,
-//   },
-// ];
-
-// function displayBeasts(array) {
-//   for (let beast of array) {
-//     let img = this.image_url;
-//     let alt = "horned beast image";
-//     let title = this.title;
-//     let disc = this.description;
-
-//     return `<HornedBeasts imgSrc="${img}" alt="${alt} title="${title} />`
-//     console.log("beasts func is funcin")
-//   }
-// }
-
-// let singleVariable = displayBeasts(beastsArray);
-// console.log(singleVariable)
+import beastData from './data.json';
 
 class Main extends Component {
   
@@ -52,14 +8,28 @@ class Main extends Component {
       <>
         <h1>Main is maining</h1>
         <HornedBeasts
-          imgSrc="https://place-hold.it/300x500"
-          alt="Horned Image"
-          title="idk horned title"
+          imgSrc={beastData[0].image_url}
+          alt={beastData[0].keyword}
+          title={beastData[0].title}
+          description={beastData[0].description}
         />
         <HornedBeasts
-          imgSrc="https://place-hold.it/300x500"
-          alt="Horned Image"
-          title="idk horned title"
+          imgSrc={beastData[1].image_url}
+          alt={beastData[1].keyword}
+          title={beastData[1].title}
+          description={beastData[1].description}
+        />
+        <HornedBeasts
+          imgSrc={beastData[2].image_url}
+          alt={beastData[2].keyword}
+          title={beastData[2].title}
+          description={beastData[2].description}
+        />
+        <HornedBeasts
+          imgSrc={beastData[3].image_url}
+          alt={beastData[3].keyword}
+          title={beastData[3].title}
+          description={beastData[3].description}
         />
       </>
     );
@@ -68,16 +38,26 @@ class Main extends Component {
 
 class HornedBeasts extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      favorites: 0
+    }
+  }
+
   render() {
     return (
       <>
-        <h2>Horned title</h2>
+        <h2>{this.props.title}</h2>
         <img
           src={this.props.imgSrc}
           alt={this.props.alt}
           title={this.props.title}
+          onClick={() => this.setState({ favorites: this.state.favorites + 1 })}
         />
-        <p>p description of the beast</p>
+        <p>&hearts; {this.state.favorites}</p>
+        <p>{this.props.description}</p>
       </>
     );
   }
